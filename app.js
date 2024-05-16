eda6febaa388da760dce5a8986e446e605f28ae5
Rachel-2013 <rachelvrws20a1005@ssvminstitutions.ac.in>
2024-05-16 18:39:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var dark = document.getElementById("dark");
+var light = document.getElementById("Light");
+
+function darkMode()
+{
+    document.body.style.backgroundColor="black";
+    document.body.style.color = "white";
+}
+
+function lightMode()
+{
+    document.body.style.backgroundColor="white";
+    document.body.style.color = "black";
+}
